9b5657d5c8d015b98d813d7e9d7dddd859cf4f33
remi-martinez <remi.martinez74500@outlook.fr>
2022-05-06 16:21:31 +0200

    bras robotique

--- kohonen.py
+++ kohonen.py
@@ -239,17 +239,69 @@ class SOM:
         # On renvoie l'erreur de quantification vectorielle moyenne
         return s / nsamples
 
+    def closest_motor_neuron(self, t1, t2):
+        """
+        @summary: Calcul du neurone le plus proche du point passé en paramètre, dans la carte position motrice
+        @param t1 : coordonnée theta 1 du point choisi
+        @param t2 : coordonnée theta 2 du point choisi
+        """
+        t = numpy.array((t1, t2))  # point choisi
+        w = numpy.array(self.weightsmap)  # tous les poids
+        n = numpy.resize(w[1][1], (1, 2))  # neurones motricité
+        neuron_x, neuron_y = 0, 0  # coordonnées x, y du neurone qui sera trouvé
+        min_dist = np.linalg.norm(t - n)  # distance euclidienne
+        for i in range(w.shape[0]):
+            for j in range(w.shape[1]):
+                n = numpy.resize(w[i][j], (1, 2))
+                current_dist = numpy.linalg.norm(t - n)
+                if current_dist < min_dist:
+                    neuron_x = i # définir la pos x du neurone
+                    neuron_y = j # définir la pos y du neurone
+                    min_dist = current_dist
+        return w[neuron_x][neuron_y]
+
+    def closest_arm_neuron(self, x1, x2):
+        """
+        @summary: Calcul du neurone le plus proche du point passé en paramètre, dans la carte position spatiale du bras
+        @param x1 : coordonnée x1 du point choisi
+        @param x2 : coordonnée y2 du point choisi
+        """
+        t = np.array((x1, x2))  # point choisi
+        w = np.array(self.weightsmap)  # tous les poids
+        n = np.array_split(w[1][1], 2)  # tous les neurones
+        neuron_x, neuron_y = 0, 0  # coordonnées x, y du neurone qui sera trouvé
+        min_dist = np.linalg.norm(t - n)  # distance euclidienne
+        for i in range(w.shape[0]):
+            for j in range(w.shape[1]):
+                n = np.array_split(w[i][j], 2)
+                current_dist = np.linalg.norm(t - n)
+                if current_dist < min_dist:
+                    neuron_x = i  # définir la pos x du neurone
+                    neuron_y = j  # définir la pos y du neurone
+                    min_dist = current_dist
+        return w[neuron_x][neuron_y]
+
+    def motor_pos_prediction(self, x1, x2):
+        selected_neuron = self.closest_arm_neuron(x1, x2)
+        print('Position motrice estimée pour le point (x1,x2) = (' + str(x1) + ',' + str(x2) + ') ', end='')
+        print('=> (θ1, θ2) = (' + str(selected_neuron[0]) + ',' + str(selected_neuron[1]) + ')')
+
+    def arm_pos_prediction(self, t1, t2):
+        selected_neuron = self.closest_motor_neuron(t1, t2)
+        print('Position spatiale du bras estimée pour le point (θ1,θ2) = (' + str(t1) + ',' + str(t2) + ') ', end='')
+        print('=> (x1, x2) = (' + str(selected_neuron[0]) + ',' + str(selected_neuron[1]) + ')')
+
 
 # -----------------------------------------------------------------------------
 if __name__ == '__main__':
     # Création d'un réseau avec une entrée (2,1) et une carte (10,10)
     # TODO mettre à jour la taille des données d'entrée pour les données robotiques
-    network = SOM((2, 1), (10, 10))
+    network = SOM((4, 1), (10, 10))
     # PARAMÈTRES DU RÉSEAU
     # Taux d'apprentissage
     ETA = 0.05
     # Largeur du voisinage
-    SIGMA = 1.4
+    SIGMA = 2
     # Nombre de pas de temps d'apprentissage
     N = 30000
     # Affichage interactif de l'évolution du réseau
@@ -260,44 +312,44 @@ if __name__ == '__main__':
     # DONNÉES D'APPRENTISSAGE
     # Nombre de données à générer pour les ensembles 1, 2 et 3
     # TODO décommenter les données souhaitées
-    nsamples = 1200
+    nsamples = 1000
     # Ensemble de données 1
-    samples = numpy.random.random((nsamples, 2, 1)) * 2 - 1
+    # samples = numpy.random.random((nsamples, 2, 1)) * 2 - 1
     # Ensemble de données 2
-    #  samples1 = -numpy.random.random((nsamples//3,2,1))
-    #  samples2 = numpy.random.random((nsamples//3,2,1))
-    #  samples2[:,0,:] -= 1
-    #  samples3 = numpy.random.random((nsamples//3,2,1))
-    #  samples3[:,1,:] -= 1
-    #  samples = numpy.concatenate((samples1,samples2,samples3))
+    # samples1 = -numpy.random.random((nsamples//3,2,1))
+    # samples2 = numpy.random.random((nsamples//3,2,1))
+    # samples2[:,0,:] -= 1
+    # samples3 = numpy.random.random((nsamples//3,2,1))
+    # samples3[:,1,:] -= 1
+    # samples = numpy.concatenate((samples1,samples2,samples3))
     # Ensemble de données 3
-    #  samples1 = numpy.random.random((nsamples//2,2,1))
-    #  samples1[:,0,:] -= 1
-    #  samples2 = numpy.random.random((nsamples//2,2,1))
-    #  samples2[:,1,:] -= 1
-    #  samples = numpy.concatenate((samples1,samples2))
+    # samples1 = numpy.random.random((nsamples//2,2,1))
+    # samples1[:,0,:] -= 1
+    # samples2 = numpy.random.random((nsamples//2,2,1))
+    # samples2[:,1,:] -= 1
+    # samples = numpy.concatenate((samples1,samples2))
     # Ensemble de données robotiques
-    #  samples = numpy.random.random((nsamples,4,1))
-    #  samples[:,0:2,:] *= numpy.pi
-    #  l1 = 0.7
-    #  l2 = 0.3
-    #  samples[:,2,:] = l1*numpy.cos(samples[:,0,:])+l2*numpy.cos(samples[:,0,:]+samples[:,1,:])
-    #  samples[:,3,:] = l1*numpy.sin(samples[:,0,:])+l2*numpy.sin(samples[:,0,:]+samples[:,1,:])
+    samples = numpy.random.random((nsamples,4,1))
+    samples[:,0:2,:] *= numpy.pi
+    l1 = 0.7
+    l2 = 0.3
+    samples[:,2,:] = l1*numpy.cos(samples[:,0,:])+l2*numpy.cos(samples[:,0,:]+samples[:,1,:])
+    samples[:,3,:] = l1*numpy.sin(samples[:,0,:])+l2*numpy.sin(samples[:,0,:]+samples[:,1,:])
     # Affichage des données (pour les ensembles 1, 2 et 3)
+    # plt.figure()
+    # plt.scatter(samples[:, 0, 0], samples[:, 1, 0])
+    # plt.xlim(-1, 1)
+    # plt.ylim(-1, 1)
+    # plt.suptitle('Donnees apprentissage')
+    # plt.show()
+    # Affichage des données (pour l'ensemble robotique)
     plt.figure()
-    plt.scatter(samples[:, 0, 0], samples[:, 1, 0])
-    plt.xlim(-1, 1)
-    plt.ylim(-1, 1)
+    plt.subplot(1,2,1)
+    plt.scatter(samples[:,0,0].flatten(),samples[:,1,0].flatten(),c='k')
+    plt.subplot(1,2,2)
+    plt.scatter(samples[:,2,0].flatten(),samples[:,3,0].flatten(),c='k')
     plt.suptitle('Donnees apprentissage')
     plt.show()
-    # Affichage des données (pour l'ensemble robotique)
-    #  plt.figure()
-    #  plt.subplot(1,2,1)
-    #  plt.scatter(samples[:,0,0].flatten(),samples[:,1,0].flatten(),c='k')
-    #  plt.subplot(1,2,2)
-    #  plt.scatter(samples[:,2,0].flatten(),samples[:,3,0].flatten(),c='k')
-    #  plt.suptitle('Donnees apprentissage')
-    #  plt.show()
 
     # SIMULATION
     # Affichage des poids du réseau
@@ -325,7 +377,8 @@ if __name__ == '__main__':
             plt.clf()
             # Remplissage de la figure
             # TODO à remplacer par scatter_plot_2 pour les données robotiques
-            network.scatter_plot(True)
+            # network.scatter_plot(True)
+            network.scatter_plot_2(True)
             # Affichage du contenu de la figure
             plt.pause(0.00001)
             plt.draw()
@@ -337,3 +390,6 @@ if __name__ == '__main__':
     network.plot()
     # Affichage de l'erreur de quantification vectorielle moyenne après apprentissage
     print("erreur de quantification vectorielle moyenne ", network.MSE(samples))
+
+    network.arm_pos_prediction(1.5, 1.5)
+    network.motor_pos_prediction(0, 0.5)
